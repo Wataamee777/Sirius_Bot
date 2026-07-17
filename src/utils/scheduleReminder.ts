13e6a506545454d07d0f7dc2be04faf8cd11ccf0
わたあめえ <Wataamee777@gmail.com>
2026-07-17 16:54:16 +0900

    Refactor scheduleReminder function parameters
    Refactor scheduleReminder to use serverId and channelId parameters and save reminder to the database.

--- src/utils/scheduleReminder.ts
+++ src/utils/scheduleReminder.ts
@@ -1,16 +1,19 @@
-import type { Message } from "discord.js";
+import { prisma } from "@/database/prisma";
 
-export default function scheduleReminder(
-	channel: Message["channel"],
-	content: string,
+export default async function scheduleReminder(
+	serverId: string,
+	channelId: string,
 	delay: number,
 ) {
-	if (delay < 0) throw new RangeError("Delay must be a non-negative number");
-	if (!channel.isTextBased() || !channel.isSendable()) {
-		throw new Error("Channel is not sendable");
+	if (delay < 0) {
+		throw new RangeError("Delay must be a non-negative number");
 	}
 
-	setTimeout(() => {
-		channel.send(content);
-	}, delay);
+	await prisma.bumpReminder.create({
+		data: {
+			serverId,
+			channelId,
+			remindAt: new Date(Date.now() + delay),
+		},
+	});
 }
